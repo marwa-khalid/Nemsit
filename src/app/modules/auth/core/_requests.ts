8f23c2f344f8be8df714471ef1a3529b4263aed3
Marwa Khalid <marwakhalid558@gmail.com>
2024-05-23 14:36:51 -0700

    api login

--- src/app/modules/auth/core/_requests.ts
+++ src/app/modules/auth/core/_requests.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
 import { AuthModel, UserModel } from "./_models";
-
 const API_URL = import.meta.env.VITE_APP_API_URL;
 
 export const GET_USER_BY_ACCESSTOKEN_URL = `${API_URL}/verify_token`;
-export const LOGIN_URL = `${API_URL}/login`;
+export const LOGIN_URL = `https://api.invoicehippo.nl/api/v1/authorization/authorize`;
 export const REGISTER_URL = `${API_URL}/register`;
 export const REQUEST_PASSWORD_URL = `${API_URL}/forgot_password`;
 
 // Server should return AuthModel
 export function login(email: string, password: string) {
   return axios.post<AuthModel>(LOGIN_URL, {
-    email,
+    userName:email,
     password,
+    languageType: 2,
+    deviceType: 1,
+    deviceId: "Windows"
   });
 }
 
