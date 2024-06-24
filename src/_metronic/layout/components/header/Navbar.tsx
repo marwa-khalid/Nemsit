0453244f932d5ca303a8c17d41f7cd5d5785fb6c
Marwa Khalid <marwakhalid558@gmail.com>
2024-06-24 18:23:39 +0500

    languages

--- src/_metronic/layout/components/header/Navbar.tsx
+++ src/_metronic/layout/components/header/Navbar.tsx
@@ -94,7 +94,7 @@ const Navbar = () => {
             userAvatarClass,
             largeBtnClass
           )}
-          data-kt-menu-trigger="{default: 'click'}"
+          data-kt-menu-trigger="{default: 'hover'}"
           data-kt-menu-attach="parent"
           data-kt-menu-placement="bottom-end"
         >
